d3ea6d029e304337523f806f7318c67820ac4eb2
Ignat <romign@gmail.com>
2023-04-26 20:14:43 +0100

    cover contracts with tests

---
+++ test/CSHToken.ts
@@ -0,0 +1,81 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+import { ContractFactory, Contract, Wallet } from "ethers";
+
+interface DeployTokenFixtureResult {
+  CSHToken: ContractFactory;
+  cshToken: Contract;
+  owner: Wallet;
+  addr1: Wallet;
+  addr2: Wallet;
+  codesFactory: Wallet;
+}
+const deployTokenFixture = async (): Promise<DeployTokenFixtureResult> => {
+  const CSHToken = await ethers.getContractFactory("CSHToken");
+  const [owner, addr1, addr2, codesFactory] = await ethers.getSigners();
+  const cshToken = await CSHToken.deploy();
+  await cshToken.deployed();
+  await cshToken.connect(owner).initialize();
+  return { CSHToken, cshToken, owner, addr1, addr2, codesFactory };
+};
+
+describe("CSHToken", () => {
+  let cshToken: Contract,
+    owner: Wallet,
+    addr1: Wallet,
+    addr2: Wallet,
+    codesFactory: Wallet;
+
+  beforeEach(async () => {
+    ({ cshToken, owner, addr1, addr2, codesFactory } = await loadFixture(
+      deployTokenFixture
+    ));
+  });
+
+  it("Should have the correct name and symbol", async () => {
+    expect(await cshToken.name()).to.equal("CryptoCash Token");
+    expect(await cshToken.symbol()).to.equal("CSH");
+  });
+
+  it("Should set the owner upon initialization", async () => {
+    expect(await cshToken.owner()).to.equal(owner.address);
+  });
+
+  it("Should allow the owner to set the AuthorizedMinter", async () => {
+    await cshToken.connect(owner).setAuthorizedMinter(codesFactory.address);
+    expect(await cshToken.getAuthorizedMinter()).to.equal(codesFactory.address);
+  });
+
+  it("Should not allow non-owners to set the AuthorizedMinter", async () => {
+    await expect(
+      cshToken.connect(addr1).setAuthorizedMinter(codesFactory.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("Should allow the owner to change the AuthorizedMinter", async () => {
+    await cshToken.connect(owner).setAuthorizedMinter(codesFactory.address);
+    await cshToken.connect(owner).setAuthorizedMinter(addr1.address);
+    expect(await cshToken.getAuthorizedMinter()).to.equal(addr1.address);
+  });
+
+  it("Should allow the AuthorizedMinter to mint tokens", async () => {
+    await cshToken.connect(owner).setAuthorizedMinter(codesFactory.address);
+    await cshToken.connect(codesFactory).mint(addr1.address, 1000);
+    expect(await cshToken.balanceOf(addr1.address)).to.equal(1000);
+  });
+
+  it("Should not allow non-AuthorizedMinters to mint tokens", async () => {
+    await expect(
+      cshToken.connect(addr1).mint(addr2.address, 1000)
+    ).to.be.revertedWith("Only AuthorizedMinter can mint tokens");
+  });
+
+  it("Should emit the CodesFactoryChanged event when setting a new AuthorizedMinter", async () => {
+    await expect(
+      cshToken.connect(owner).setAuthorizedMinter(codesFactory.address)
+    )
+      .to.emit(cshToken, "CodesFactoryChanged")
+      .withArgs(ethers.constants.AddressZero, codesFactory.address);
+  });
+});
